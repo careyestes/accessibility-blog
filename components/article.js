7e80ffdae2d3e434450b6cda72473149988b8953
Carey Estes <carey.estes@omf.com>
2022-10-10 12:34:05 -0500

    Added env variable for prod

--- components/article.js
+++ components/article.js
@@ -2,10 +2,11 @@ import Link from "next/link";
 import Image from "next/image";
 import classNames from "classnames";
 import Date from '../components/date';
+import { prefix } from '../utils/prefix.js'
 
 export default function Article({type, date, index=null, title, snippet, id=null, isEven=null, content=null, image=null}) {
   return (
-    <article className={ classNames("Article", type, isEven) } style={{ backgroundImage: type === "-hero" ? `url(${image})` : type === "-single_post" ? `url(${image})` : "none"}}>
+    <article className={ classNames("Article", type, isEven) } style={{ backgroundImage: type === "-hero" ? `url(${prefix + image})` : type === "-single_post" ? `url(${prefix + image})` : "none"}}>
       <div className="article_wrapper">
         <Date dateString={date} />
         <h2 className={ classNames({ 'heading_large' : index === 0  }) }>
@@ -13,7 +14,7 @@ export default function Article({type, date, index=null, title, snippet, id=null
           { image ? 
             <span className="image-container">
               <Image 
-                src={image}
+                src={prefix + image}
                 layout='fill'
                 objectFit='cover'
               />
