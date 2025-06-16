b88f7826d7311bb2adb761b6f6389121dcd4d5cb
Carey Estes <carey@careyestes.com>
2025-06-16 11:43:25 -0500

    UPdated images opt

--- next.config.js
+++ next.config.js
@@ -2,19 +2,20 @@
 const path = require('path');
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  // preserve your existing basePath + assetPrefix
+module.exports = {
+  // preserve your basePath + assetPrefix
   basePath: process.env.NEXT_PUBLIC_BASE_PATH,
   assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
 
   reactStrictMode: true,
 
-  // webpack alias to redirect every `import Image from 'next/image'`
-  // to our own lightweight wrapper
   webpack(config) {
-    config.resolve.alias['next/image'] = path.resolve(__dirname, 'lib/unoptimized-image.js');
+    // whenever code does `import Image from 'next/image'`,
+    // substitute it with our unoptimized wrapper
+    config.resolve.alias['next/image'] = path.resolve(
+      __dirname,
+      'lib/unoptimized-image.js'
+    );
     return config;
   },
 };
-
-module.exports = nextConfig;
